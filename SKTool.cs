03160c039ea1220a67564d5c9def05079047b541
Soumakakami <31908694+Soumakakami@users.noreply.github.com>
2020-01-24 11:36:24 +0900

    Add files via upload

--- SKTool.cs
+++ SKTool.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 using System;
-using System.Linq;
 
 /// <summary>
 /// Unity用に拡張した機能です
@@ -50,19 +49,68 @@ namespace SKTool.Unity
 			}
 		}
 	}
+	public static class GameObjectExtension
+	{
+		/// <summary>
+		/// 自身から一番近い位置のオブジェクトを返します
+		/// </summary>
+		/// <param name="_self">自身</param>
+		/// <param name="_list">チェックしたいリスト</param>
+		/// <returns></returns>
+		public static GameObject GetClosestObjectList(this GameObject _self,List<GameObject> _list)
+		{
+			GameObject gameObject=null;
+			float dis=Vector3.Distance(_self.transform.position,_list[0].transform.position);
+			for (int i = 1; i < _list.Count; i++)
+			{
+				if (Vector3.Distance(_self.transform.position,_list[i].transform.position)<=dis)
+				{
+					dis = Vector3.Distance(_self.transform.position, _list[i].transform.position);
+					gameObject = _list[i];
+				}
+			}
+			return gameObject;
+		}
+		/// <summary>
+		/// 自身から一番近い位置のオブジェクトを返します
+		/// </summary>
+		/// <param name="_self">自身</param>
+		/// <param name="_array">チェックしたい配列</param>
+		/// <returns></returns>
+		public static GameObject GetClosestObjectArray(this GameObject _self, GameObject[] _array)
+		{
+			GameObject gameObject = null;
+			float dis = Vector3.Distance(_self.transform.position, _array[0].transform.position);
+			for (int i = 1; i < _array.Length; i++)
+			{
+				if (Vector3.Distance(_self.transform.position, _array[i].transform.position) <= dis)
+				{
+					dis = Vector3.Distance(_self.transform.position, _array[i].transform.position);
+					gameObject = _array[i];
+				}
+			}
+			return gameObject;
+		}
+
+	}
+
 }
 
 namespace SKTool
 {
 	public static class BasicClass
 	{
+		/// <summary>
+		/// 入れ替え
+		/// </summary>
+		/// <typeparam name="T">なんでいいよ</typeparam>
+		/// <param name="x">入れ替え</param>
+		/// <param name="y">入れ替え</param>
 		public static void Exchange<T>(ref T x,ref T y)where T:struct
 		{
 			T z = x;
 			x = y;
 			y = z;
-			
 		}
-		
-	}		
+	}
 }
